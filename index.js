927b30e3b11d0c30182250ea289d6e00ffe248eb
olekunger <ounger@inbox.ru>
2018-12-19 16:59:50 +0300

    -

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./app')
-// const http = require('http');
-// const server = http.createServer(app);
-// const io = require('socket.io').listen(server);
-// require('./config/soket')(io)
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+require('./config/soket')(io)
 
 const port = process.env.PORT || 4200
-app.listen(port, () => console.log(`Server running on port ${port}`))
+server.listen(port, () => console.log(`Server running on port ${port}`))
